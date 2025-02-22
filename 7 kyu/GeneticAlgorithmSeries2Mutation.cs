e8e20cd1dcc6f1aec7770ee3078a46e3f3791eb4
Truman Hogg <hoggtruman@gmail.com>
2025-02-22 15:17:59 +0000

    Add GeneticAlgorithmSeries2Mutation

---
+++ 7 kyu/GeneticAlgorithmSeries2Mutation.cs
@@ -0,0 +1,23 @@
+﻿// https://www.codewars.com/kata/567b39b27d0a4606a5000057
+
+using System.Text;
+using System;
+
+class GeneticAlgorithmSeries2Mutation
+{
+    public string Mutate(string chromosome, double probability)
+    {
+        Random rnd = new();
+        StringBuilder sb = new(chromosome);
+
+        for (int i = 0; i < chromosome.Length; ++i)
+        {
+            if (rnd.NextDouble() <= probability)
+            {
+                sb[i] = sb[i] == '0' ? '1' : '0';
+            }
+        }
+
+        return sb.ToString();
+    }
+}
