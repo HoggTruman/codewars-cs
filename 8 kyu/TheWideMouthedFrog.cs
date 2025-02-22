492742ebb4ca27d085ae06086e59920f515a4c46
Truman Hogg <hoggtruman@gmail.com>
2025-02-22 15:36:07 +0000

    Add TheWideMouthedFrog

---
+++ 8 kyu/TheWideMouthedFrog.cs
@@ -0,0 +1,9 @@
+﻿// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+
+class TheWideMouthedFrog
+{
+    public static string MouthSize(string animal)
+    {
+        return animal.ToLower() == "alligator" ? "small" : "wide";
+    }
+}
