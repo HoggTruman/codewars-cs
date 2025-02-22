3c6598367e1de2dca5d4fd2a664124df34c4da70
Truman Hogg <hoggtruman@gmail.com>
2025-02-22 15:56:26 +0000

    Add DoesMyNumberLookBigInThis

---
+++ 6 kyu/DoesMyNumberLookBigInThis.cs
@@ -0,0 +1,20 @@
+﻿// https://www.codewars.com/kata/5287e858c6b5a9678200083c
+
+
+class DoesMyNumberLookBigInThis
+{
+    public static bool Narcissistic(int value)
+    {
+        int numDigits = (int)Math.Log10(value) + 1;
+        int sum = 0;
+        int remaining = value;        
+
+        while (remaining > 0)
+        {
+            sum += (int)Math.Pow(remaining % 10, numDigits);
+            remaining /= 10;
+        }
+
+        return sum == value;
+    }
+}
