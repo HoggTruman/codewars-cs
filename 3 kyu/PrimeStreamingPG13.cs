28c834650fb6400531e150e44b2f53f61fe2db35
Truman Hogg <hoggtruman@gmail.com>
2025-02-24 17:28:44 +0000

    Add PrimeStreamingPG13

---
+++ 3 kyu/PrimeStreamingPG13.cs
@@ -0,0 +1,28 @@
+﻿// https://www.codewars.com/kata/5519a584a73e70fa570005f5
+
+namespace PrimeStreamingPG13;
+
+using System.Collections.Generic;
+public class Primes
+{
+    public static IEnumerable<int> Stream()
+    {
+        const long n = 20_000_000;
+        bool[] isPrime = new bool[n + 1];
+        for (long i = 2; i < isPrime.Length; ++i) {
+            isPrime[i] = true;
+        }            
+
+        for (long p = 2; p <= n; ++p)
+        {
+            if (isPrime[p])
+            {
+                yield return (int)p;
+
+                for (long k = p * p; k <= n; k += p) {
+                    isPrime[k] = false;
+                }
+            }
+        }
+    }
+}
