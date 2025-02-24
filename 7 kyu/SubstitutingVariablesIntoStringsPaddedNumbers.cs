3b2a9bd5554dd7c1068c52b1822a2f38fd5eb8ae
Truman Hogg <hoggtruman@gmail.com>
2025-02-24 17:50:45 +0000

    Add SubstitutingVariablesIntoStringsPaddedNumbers

---
+++ 7 kyu/SubstitutingVariablesIntoStringsPaddedNumbers.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/51c89385ee245d7ddf000001
+
+namespace SubstitutingVariablesIntoStringsPaddedNumbers;
+
+public class PaddedNumbers
+{
+    public static string Solution(int value)
+    {
+        return $"Value is {value:D5}";
+    }
+}
