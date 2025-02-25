c34d88d04f897b645e0bd214b4df316a96114239
Truman Hogg <hoggtruman@gmail.com>
2025-02-25 17:17:24 +0000

    Add NumberOfTrailingZerosOfNFactorial

---
+++ 5 kyu/NumberOfTrailingZerosOfNFactorial.cs
@@ -0,0 +1,17 @@
+﻿// https://www.codewars.com/kata/52f787eb172a8b4ae1000a34
+
+namespace NumberOfTrailingZerosOfNFactorial;
+
+public static class Kata 
+{
+    public static int TrailingZeros(int n)
+    {
+        int fiveFactorCount = 0;
+        for (int d = 5; d <= n; d *= 5)
+        {
+            fiveFactorCount += n / d;
+        }
+
+        return fiveFactorCount;
+    }
+}
