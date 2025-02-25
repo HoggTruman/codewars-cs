f5627cd20afdfa44406ba53837906d5ad078753d
Truman Hogg <hoggtruman@gmail.com>
2025-02-25 11:09:44 +0000

    Add BoiledEggs

---
+++ 7 kyu/BoiledEggs.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/52b5247074ea613a09000164
+
+namespace BoiledEggs;
+
+public class Kata
+{
+    public static int CookingTime(int eggs)
+    {
+        return (eggs + 7) / 8 * 5;
+    }
+}
