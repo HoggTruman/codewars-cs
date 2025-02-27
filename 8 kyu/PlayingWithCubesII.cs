c3f2648edd3596771282db27088a41f3b8cdb8e4
Truman Hogg <hoggtruman@gmail.com>
2025-02-27 14:11:48 +0000

    Add PlayingWithCubesII

---
+++ 8 kyu/PlayingWithCubesII.cs
@@ -0,0 +1,23 @@
+﻿// https://www.codewars.com/kata/55c0ac142326fdf18d0000af
+
+namespace PlayingWithCubesII;
+
+public class Cube
+{
+    private int Side;
+  
+    public Cube(int side=0)
+    {
+        Side = side > 0? side: -side;
+    }
+  
+    public int GetSide()
+    {
+        return this.Side;
+    }
+  
+    public void SetSide(int s)
+    {
+        this.Side = s;
+    }
+}
