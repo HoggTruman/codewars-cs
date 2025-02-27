f69a54a09a5891614dbb1fe5c9b7889aa3040b29
Truman Hogg <hoggtruman@gmail.com>
2025-02-27 14:06:11 +0000

    Add ColorGhost

---
+++ 8 kyu/ColorGhost.cs
@@ -0,0 +1,18 @@
+﻿// https://www.codewars.com/kata/53f1015fa9fe02cbda00111a
+
+namespace ColorGhost;
+
+using System;
+
+public class Ghost
+{
+    private static string[] colors = { "white", "red", "purple", "yellow" };
+    private string _color;
+
+    public Ghost()
+    {
+        _color = colors[new Random().Next(colors.Length)];        
+    }
+
+    public string GetColor() => _color;        
+}
