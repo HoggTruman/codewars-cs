978c29162dd994c6817bf9d78c721ac0996af47c
Truman Hogg <hoggtruman@gmail.com>
2025-03-03 22:15:53 +0000

    Add StringTemplatesBugFixing5

---
+++ 8 kyu/StringTemplatesBugFixing5.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/55c90cad4b0fe31a7200001f
+
+namespace StringTemplatesBugFixing5;
+
+using System;
+
+public static class Kata 
+{
+    public static string buildString(string[] args)
+    {
+      return String.Format("I like {0}!", String.Join(", ", args));
+    }
+}
