9c9002f610f73107fce1d889154f433df36630a2
Truman Hogg <hoggtruman@gmail.com>
2025-03-04 18:03:52 +0000

    Add TakeTheDerivative

---
+++ 8 kyu/TakeTheDerivative.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/5963c18ecb97be020b0000a2
+
+namespace TakeTheDerivative;
+
+public class Kata
+{
+    public static string Derive(double coefficient, double exponent)
+    {
+        return $"{coefficient * exponent}x^{exponent - 1}";
+    }
+}
