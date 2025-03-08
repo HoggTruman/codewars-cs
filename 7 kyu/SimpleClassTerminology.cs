2dc2aa1d94f8f40eb4a44a0573bfbac9cbf2065f
Truman Hogg <hoggtruman@gmail.com>
2025-03-08 14:39:25 +0000

    Add SimpleClassTerminology

---
+++ 7 kyu/SimpleClassTerminology.cs
@@ -0,0 +1,15 @@
+﻿// https://www.codewars.com/kata/58cfe6c9f9dffe0f5100017f
+
+namespace SimpleClassTerminology;
+
+public class DemoClass
+{
+    private int _privateField;
+    public string PublicField = "None";
+    public int LimitedProperty { get; private set; }
+
+    public DemoClass(int value)
+    {
+        _privateField = value;
+    }
+}
