965744871e3f49b77a251a963ea1891eca2941d0
Truman Hogg <hoggtruman@gmail.com>
2025-03-10 20:43:27 +0000

    Add Reflection3AddTheMemberResults

---
+++ 5 kyu/Reflection3AddTheMemberResults.cs
@@ -0,0 +1,48 @@
+﻿// https://www.codewars.com/kata/57722700c37fc4cc99000033
+
+namespace Reflection3AddTheMemberResults;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public static class Reflection
+{
+    public static string ConcatStringMembers(object TestObject)
+    {
+        if (TestObject == null) 
+            return "";
+
+        HashSet<string> results = [];
+
+        foreach (MethodInfo method in TestObject.GetType().GetMethods())
+        {
+            if (method.ReturnType == typeof(string) &&
+                method.GetParameters().Length == 0 &&
+                method.Name != "ToString")
+            {
+                results.Add((string)method.Invoke(TestObject, null));
+            }
+        }
+
+        foreach (PropertyInfo prop in TestObject.GetType().GetProperties())
+        {
+            if (prop.PropertyType == typeof(string))
+            {
+                results.Add((string)prop.GetValue(TestObject, null));
+            }
+        }
+
+        foreach (FieldInfo field in TestObject.GetType().GetFields())
+        {
+            Console.WriteLine(field);
+            if (field.FieldType == typeof(string))
+            {
+                results.Add((string)field.GetValue(TestObject));
+            }
+        }
+
+        return String.Join("", results.OrderByDescending(x => x.Length).ThenBy(x => x).ToList());
+    }
+}
