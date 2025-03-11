bafa11c484344eda7ac62fff2ae6413d130ef432
Truman Hogg <hoggtruman@gmail.com>
2025-03-11 19:18:00 +0000

    Add Vectors1

---
+++ 7 kyu/Vectors1.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/5523b97ac8f5025c45000900
+
+namespace Vectors1;
+
+using System;
+
+public class Vector3D
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+    public double Length { get => Math.Sqrt(X * X + Y * Y + Z * Z); }
+}
