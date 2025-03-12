6bac993480880472ffcc50dca84c9e94be9c1a8d
Truman Hogg <hoggtruman@gmail.com>
2025-03-12 17:22:47 +0000

    Add ClassyExtensions

---
+++ 8 kyu/ClassyExtensions.cs
@@ -0,0 +1,18 @@
+﻿// https://www.codewars.com/kata/55a14aa4817efe41c20000bc
+
+
+
+namespace ClassyExtensions;
+
+//public class Cat : Animal
+//{
+//    public Cat(string name) : base(name)
+//    {
+    
+//    }
+  
+//    public string Speak()
+//    {
+//        return $"{Name} meows.";
+//    }
+//}
