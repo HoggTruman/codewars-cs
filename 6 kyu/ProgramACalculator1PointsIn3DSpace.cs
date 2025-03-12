abb959db3c47f601c8207f6f1fdf1867e0116d38
Truman Hogg <hoggtruman@gmail.com>
2025-03-12 17:13:49 +0000

    Add ProgramACalculator1PointsIn3DSpace

---
+++ 6 kyu/ProgramACalculator1PointsIn3DSpace.cs
@@ -0,0 +1,36 @@
+﻿// https://www.codewars.com/kata/58e46873c45e9af88d00006a
+
+namespace ProgramACalculator1PointsIn3DSpace;
+
+using System;
+
+public class Point
+{
+    public double X { get; }
+    public double Y { get; }
+    public double Z { get; }
+
+    
+
+    public Point(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static Point GetOrigin()
+    {
+        return new Point(0, 0 ,0);
+    }
+
+    public double GetDistanceFromOrigin()
+    {
+        return Math.Sqrt(X * X + Y * Y + Z * Z);
+    }
+
+    public double GetDistanceFromPoint(Point p)
+    {
+        return Math.Sqrt(Math.Pow(X - p.X, 2) + Math.Pow(Y - p.Y, 2) + Math.Pow(Z - p.Z, 2));
+    }
+}
