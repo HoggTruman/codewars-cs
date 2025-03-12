28fe007ccde6f2ed202f130fffa352abe73c8222
Truman Hogg <hoggtruman@gmail.com>
2025-03-12 17:26:55 +0000

    Add ClassyClasses

---
+++ 8 kyu/ClassyClasses.cs
@@ -0,0 +1,17 @@
+﻿// https://www.codewars.com/kata/55a144eff5124e546400005a
+
+namespace ClassyClasses;
+
+public class Person
+{  
+    private readonly string _name;
+    private readonly int _age;
+
+    public string Info => $"{_name}s age is {_age}";
+
+    public Person(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+}
