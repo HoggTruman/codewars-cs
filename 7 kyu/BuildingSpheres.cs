8a2bbef587519b9a44104f6df04b6367f72c7a1b
Truman Hogg <hoggtruman@gmail.com>
2025-03-13 19:53:09 +0000

    Add BuildingSpheres

---
+++ 7 kyu/BuildingSpheres.cs
@@ -0,0 +1,22 @@
+﻿// https://www.codewars.com/kata/55c1d030da313ed05100005d/csharp
+
+namespace BuildingSpheres;
+
+using System;
+public class Sphere
+{
+	private readonly int _radius;
+	private readonly int _mass;
+
+	public Sphere(int radius, int mass)
+	{
+		_radius = radius;
+		_mass = mass;
+	}
+
+	public int GetRadius() => _radius;
+	public int GetMass() => _mass;
+	public double GetVolume() => Math.Round(4d / 3d * Math.PI * Math.Pow(_radius, 3), 5);
+	public double GetSurfaceArea() => Math.Round(4d * Math.PI * Math.Pow(_radius, 2), 5);
+	public double GetDensity() => Math.Round(_mass / GetVolume(), 5);
+}
