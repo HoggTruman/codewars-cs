1d928c58a6e193acfbbea53f822d7985e9d8ffbf
Truman Hogg <hoggtruman@gmail.com>
2025-03-14 20:05:16 +0000

    Add OOPObjectOrientedPiracy

---
+++ 8 kyu/OOPObjectOrientedPiracy.cs
@@ -0,0 +1,17 @@
+﻿// https://www.codewars.com/kata/54fe05c4762e2e3047000add
+
+namespace OOPObjectOrientedPiracy;
+
+public class Ship
+{
+    public int Draft;
+    public int Crew;
+  
+    public Ship(int draft, int crew)
+    {
+        Draft = draft;
+        Crew = crew;
+    }
+  
+    public bool IsWorthIt() => Draft - 1.5 * Crew > 20;
+}
