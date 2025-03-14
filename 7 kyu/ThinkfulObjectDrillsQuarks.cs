7ebd5798b8741e609fd555b9218fdf33003c590e
Truman Hogg <hoggtruman@gmail.com>
2025-03-14 18:10:23 +0000

    Add ThinkfulObjectDrills

---
+++ 7 kyu/ThinkfulObjectDrillsQuarks.cs
@@ -0,0 +1,21 @@
+﻿// https://www.codewars.com/kata/5882b052bdeafec15e0000e6
+
+namespace ThinkfulObjectDrillsQuarks;
+
+public class Quark 
+{
+    public readonly double BaryonNumber = 1d / 3d;
+    public string Color { get; private set; }
+    public string Flavor { get; private set; }
+
+    public Quark(string color, string flavor)
+    {
+        Color = color;
+        Flavor = flavor;
+    }
+
+    public void Interact(Quark q)
+    {
+        (Color, q.Color) = (q.Color, Color);
+    }
+}
