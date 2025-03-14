28403764f93e852f2d0eac1736b7a31bb4794282
Truman Hogg <hoggtruman@gmail.com>
2025-03-14 20:09:44 +0000

    Add ClassicHelloWorld

---
+++ 8 kyu/ClassicHelloWorld.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/57036f007fd72e3b77000023
+
+namespace ClassicHelloWorld;
+
+using System;
+
+class Solution 
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
