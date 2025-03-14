fc960e508ed484de931ec96e27fb78d80a9ee907
Truman Hogg <hoggtruman@gmail.com>
2025-03-14 18:16:52 +0000

    Add BrokenCounter

---
+++ 8 kyu/BrokenCounter.cs
@@ -0,0 +1,23 @@
+﻿// https://www.codewars.com/kata/526471539d52735c620000c6
+
+namespace BrokenCounter;
+
+public class Counter
+{
+    private int _value = 0;
+    public int Value
+    {
+        get => _value;
+        private set => _value = value;
+    }
+  
+    public void Increase()
+    {
+        ++Value;
+    }
+  
+    public void Reset()
+    {
+        Value = 0;
+    }
+}
