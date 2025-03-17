e566993159499d8180ce06225745c6deec7a83eb
Truman Hogg <hoggtruman@gmail.com>
2025-03-17 18:16:54 +0000

    NDimensionalVectorMagnitude

---
+++ 7 kyu/NDimensionalVectorMagnitude.cs
@@ -0,0 +1,14 @@
+﻿// https://www.codewars.com/kata/5806c2f897dba05dd900004c
+
+namespace NDimensionalVectorMagnitude;
+
+using System;
+using System.Linq;
+
+public class Kata
+{
+    public static double Magnitude(double[] vector)
+    {
+        return Math.Sqrt(vector.Sum(x => x * x));
+    }
+}
