648a1f9caedcfb0d8a178707aceb1380fafb9f30
Truman Hogg <hoggtruman@gmail.com>
2025-03-18 21:08:21 +0000

    Add SimpleCalculator

---
+++ 8 kyu/SimpleCalculator.cs
@@ -0,0 +1,20 @@
+﻿// https://www.codewars.com/kata/5810085c533d69f4980001cf
+
+namespace SimpleCalculator;
+
+using System;
+
+public class Kata
+{
+    public static double Calculator(double a, double b, char op)
+    {
+        return op switch
+        {
+            '+' => a + b,
+            '-' => a - b,
+            '*' => a * b,
+            '/' => a / b,
+            _ => throw new ArgumentException("Operator symbol must be one of +-*/")
+        };
+    }
+}
