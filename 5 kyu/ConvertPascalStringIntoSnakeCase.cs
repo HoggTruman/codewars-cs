96f2c24e5a1a4fbb50aa22de995806b03d72db19
Truman Hogg <hoggtruman@gmail.com>
2025-03-20 19:14:56 +0000

    Add ConvertPascalStringIntoSnakeCase

---
+++ 5 kyu/ConvertPascalStringIntoSnakeCase.cs
@@ -0,0 +1,24 @@
+﻿// https://www.codewars.com/kata/529b418d533b76924600085d
+
+namespace ConvertPascalStringIntoSnakeCase;
+
+using System;
+using System.Linq;
+
+public static class Kata 
+{
+    public static string ToUnderscore(int str) 
+    {
+        return str.ToString();
+    }
+
+    public static string ToUnderscore(string str) 
+    {
+        string result = "";
+        foreach (char c in str)
+        {
+            result += char.IsUpper(c)? $"_{char.ToLower(c)}": c.ToString();
+        }
+        return result.TrimStart('_');
+    }
+}
