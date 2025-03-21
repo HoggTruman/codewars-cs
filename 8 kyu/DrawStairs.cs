cb2460d7c66022ce6671f607581067c1193e388c
Truman Hogg <hoggtruman@gmail.com>
2025-03-21 20:46:48 +0000

    Add DrawStairs

---
+++ 8 kyu/DrawStairs.cs
@@ -0,0 +1,26 @@
+﻿// https://www.codewars.com/kata/5b4e779c578c6a898e0005c5
+
+namespace DrawStairs;
+
+using System.Text;
+
+public class Kata
+{
+    public static string DrawStairs(int n)
+    {
+        if (n == 0)
+        {
+            return "";
+        }
+
+        StringBuilder sb = new("I");
+        for (int i = 2; i <= n; ++i)
+        {
+            sb.Append('\n');
+            sb.Append(new string(' ', i - 1));
+            sb.Append('I');            
+        }
+
+        return sb.ToString();
+    }
+}
