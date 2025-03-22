82f402c2d4f25c62693911439b25b8d7deaa66e2
Truman Hogg <hoggtruman@gmail.com>
2025-03-22 19:25:23 +0000

    Add TemplateStrings

---
+++ 8 kyu/TemplateStrings.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/55a14f75ceda999ced000048
+
+namespace TemplateStrings;
+
+public class Templates
+{
+    public static string TempleStrings(string obj, string feature)
+    {
+        return $"{obj} are {feature}";
+    }
+}
