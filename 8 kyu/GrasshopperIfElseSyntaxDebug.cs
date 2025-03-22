dd93d1c672f5552d338133cc71b4598a3d2b9dc4
Truman Hogg <hoggtruman@gmail.com>
2025-03-22 19:36:10 +0000

    Add GrasshopperIfElseSyntaxDebug

---
+++ 8 kyu/GrasshopperIfElseSyntaxDebug.cs
@@ -0,0 +1,21 @@
+﻿// https://www.codewars.com/kata/57089707fe2d01529f00024a
+
+namespace GrasshopperIfElseSyntaxDebug;
+
+public class Player
+{
+    private int health = 100;
+    public int Health
+    {
+        get
+        {
+            return this.health;
+        }
+        set
+        {
+            health = value;
+        }
+    }
+  
+    public bool CheckAlive() => Health > 0;
+}
