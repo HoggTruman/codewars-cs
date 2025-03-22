78736fa52c8f4ae971066e307a0a15f957e4a811
Truman Hogg <hoggtruman@gmail.com>
2025-03-22 19:27:34 +0000

    Add FilteringEvenNumbersBugFixes

---
+++ 8 kyu/FilteringEvenNumbersBugFixes.cs
@@ -0,0 +1,15 @@
+﻿// https://www.codewars.com/kata/566dc566f6ea9a14b500007b
+
+namespace FilteringEvenNumbersBugFixes;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static List<int> FilterOddNumber(List<int> listOfNumbers)
+    {
+        return listOfNumbers.Where(x => x % 2 != 0).ToList();
+    }
+}
