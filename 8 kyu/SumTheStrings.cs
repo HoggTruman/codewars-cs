0a720e461e0517e1e1dca8ca8ae5fa712b62e86b
Truman Hogg <hoggtruman@gmail.com>
2025-03-22 19:25:14 +0000

    Add SumTheStrings

---
+++ 8 kyu/SumTheStrings.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/5966e33c4e686b508700002d
+
+namespace SumTheStrings;
+
+public static class Program
+{
+    public static string StringsSum(string s1, string s2)
+    {
+        return (int.Parse(s1 == ""? "0": s1) + int.Parse(s2 == ""? "0": s2)).ToString();
+    }
+}
