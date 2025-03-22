7a3289555dc1d5d52ae13cc7768f91da4be9aacd
Truman Hogg <hoggtruman@gmail.com>
2025-03-22 19:40:03 +0000

    Add SurfaceAreaAndVolumeOfABox

---
+++ 8 kyu/SurfaceAreaAndVolumeOfAbox.cs
@@ -0,0 +1,14 @@
+﻿// https://www.codewars.com/kata/565f5825379664a26b00007c
+
+namespace SurfaceAreaAndVolumeOfABox;
+
+public class Kata
+{
+    public static int[] Get_size(int w, int h, int d)
+    {
+        return [
+            2 * (w * h + w * d + h * d),
+            w * h * d
+        ];
+    }
+}
