6144b037c8a6676612ebcb17df9cb09263cc87dd
Truman Hogg <hoggtruman@gmail.com>
2025-03-22 19:25:08 +0000

    Add StringCleaning

---
+++ 8 kyu/StringCleaning.cs
@@ -0,0 +1,19 @@
+﻿// https://www.codewars.com/kata/57e1e61ba396b3727c000251
+
+namespace StringCleaning;
+using System.Text;
+
+public class Kata
+{
+    public static string StringClean(string s)
+    {
+        StringBuilder sb = new();
+        foreach (char c in s)
+        {
+            if (!char.IsDigit(c))
+                sb.Append(c);
+        }
+
+        return sb.ToString();
+    }
+}
