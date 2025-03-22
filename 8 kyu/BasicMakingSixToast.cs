4793d7f550a8adc32e9aa9ce6370a76795bdc702
Truman Hogg <hoggtruman@gmail.com>
2025-03-22 19:24:58 +0000

    Add BasicMakingSixToast

---
+++ 8 kyu/BasicMakingSixToast.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/5834fec22fb0ba7d080000e8
+
+namespace BasicMakingSixToast;
+
+using System;
+
+public class Kata
+{
+    public static int SixToast(int num)
+    {
+        return Math.Abs(6 - num);
+    }
+}
