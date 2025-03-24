d990580e5bf4798e5629a08a145cfca92f622712
Truman Hogg <hoggtruman@gmail.com>
2025-03-24 20:12:34 +0000

    Add PasswordValidator

---
+++ 7 kyu/PasswordValidator.cs
@@ -0,0 +1,17 @@
+﻿// https://www.codewars.com/kata/56a921fa8c5167d8e7000053
+
+namespace PasswordValidator;
+
+using System.Linq;
+
+public class PasswordValidator
+{
+    public static bool Password(string password)
+    {
+        return password.Length >= 8
+            && password.Any(char.IsUpper)
+            && password.Any(char.IsLower)
+            && password.Any(char.IsDigit);
+    }
+}
+
