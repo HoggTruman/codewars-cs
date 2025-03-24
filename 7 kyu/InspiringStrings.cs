2d6204a141ce40d5c67022c7bb1c07e119cf5a05
Truman Hogg <hoggtruman@gmail.com>
2025-03-24 20:17:44 +0000

    Add InspiringStrings

---
+++ 7 kyu/InspiringStrings.cs
@@ -0,0 +1,20 @@
+﻿// https://www.codewars.com/kata/5939ab6eed348a945f0007b2
+
+namespace InspiringStrings;
+
+public class Kata
+{
+    public static string LongestWord(string stringOfWords)
+    {
+        string longestWord = "";
+        foreach (string word in stringOfWords.Split())
+        {
+            if (word.Length >= longestWord.Length)
+            {
+                longestWord = word;
+            }
+        }
+
+        return longestWord;
+    }
+}
