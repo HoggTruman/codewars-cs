2e92d93f79a24d749e20351c93859fc2189a841c
Truman Hogg <hoggtruman@gmail.com>
2025-03-31 20:57:20 +0100

    Add Plural

---
+++ 8 kyu/Plural.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/52ceafd1f235ce81aa00073a
+
+namespace Plural;
+
+public class Kata
+{
+    public static bool Plural(double n)
+    {
+        return n != 1;
+    }
+}
