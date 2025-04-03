241771d6a2de1c36f30cd95546faa9f91b5c2bad
Truman Hogg <hoggtruman@gmail.com>
2025-04-03 21:20:01 +0100

    Add NotAllButSometimesAll

---
+++ 7 kyu/NotAllButSometimesAll.cs
@@ -0,0 +1,29 @@
+﻿// https://www.codewars.com/kata/564ab935de55a747d7000040
+
+namespace NotAllButSometimesAll;
+
+using System.Collections.Generic;
+using System.Text;
+
+public class Kata
+{
+    public static string Remove(string str, Dictionary<char,int> what)
+    {
+        StringBuilder sb = new();
+
+        foreach(char c in str)
+        {
+            if (what.TryGetValue(c, out int count) &&
+                count > 0)
+            {
+                --what[c];
+            }
+            else
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
