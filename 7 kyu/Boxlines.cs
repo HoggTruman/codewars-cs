ea583a1bad1714d8fa4aa90ea6454ec780edc002
Truman Hogg <hoggtruman@gmail.com>
2025-04-04 21:06:40 +0100

    Add Boxlines

---
+++ 7 kyu/Boxlines.cs
@@ -0,0 +1,14 @@
+﻿// https://www.codewars.com/kata/6129095b201d6b000e5a33f0
+
+namespace Boxlines;
+
+public static class Kata
+{
+    public static ulong F(ushort x, ushort y, ushort z) 
+    {
+        ulong xLines = x * (y + 1ul) * (z + 1ul);
+        ulong yLines = y * (x + 1ul) * (z + 1ul);
+        ulong zLines = z * (x + 1ul) * (y + 1ul);
+        return xLines + yLines + zLines;
+    }
+}
