a6a3149bd4b2a9834b1b33351bff497401db0f38
Truman Hogg <hoggtruman@gmail.com>
2025-04-05 20:30:24 +0100

    Add FindSumOfTopLeftToBottomRightDiagonals

---
+++ 7 kyu/FindSumOfTopLeftToBottomRightDiagonals.cs
@@ -0,0 +1,18 @@
+﻿// https://www.codewars.com/kata/5497a3c181dd7291ce000700
+
+namespace FindSumOfTopLeftToBottomRightDiagonals;
+
+public static class Kata
+{
+    public static int DiagonalSum(int[,] matrix)
+    {
+        //if (matrix.L)
+        int sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); ++i)
+        {
+            sum += matrix[i, i];
+        }
+
+        return sum;
+    }
+}
