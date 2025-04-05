753f85aa639c25e0ad0cd3924e5ca4223fc14ac0
Truman Hogg <hoggtruman@gmail.com>
2025-04-05 20:42:47 +0100

    Add HowGreenIsMyValley

---
+++ 7 kyu/HowGreenIsMyValley.cs
@@ -0,0 +1,17 @@
+﻿// https://www.codewars.com/kata/56e3cd1d93c3d940e50006a4
+
+namespace HowGreenIsMyValley;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Valley
+{
+    public static int[] MakeValley(int[] arr)
+    {
+        IEnumerable<int> sorted = arr.OrderDescending();
+        return sorted.Where((x, i) => i % 2 == 0)
+               .Concat(sorted.Where((x, i) => i % 2 != 0).Reverse())
+               .ToArray();
+    }
+}
