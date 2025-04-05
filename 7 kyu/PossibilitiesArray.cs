65bdbc7bba6fa599837ec56132e4bd4e734c348c
Truman Hogg <hoggtruman@gmail.com>
2025-04-05 20:48:38 +0100

    Add PossibilitiesArray

---
+++ 7 kyu/PossibilitiesArray.cs
@@ -0,0 +1,23 @@
+﻿// https://www.codewars.com/kata/59b710ed70a3b7dd8f000027
+
+namespace PossibilitiesArray;
+
+using System.Collections.Generic;
+
+public static class Kata
+{
+    public static bool IsAllPossibilities(int[] arr)
+    {
+        HashSet<int> possibilities = new();
+        foreach(int k in arr)
+        {
+            if (k < 0 || k >= arr.Length || possibilities.Contains(k))
+            {
+                return false;
+            }
+            possibilities.Add(k);
+        }
+
+        return true;
+    }
+}
