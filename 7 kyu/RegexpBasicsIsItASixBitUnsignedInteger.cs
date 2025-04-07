2a062ee4a1ec916ba612a81fb879bdd8002cbbf5
Truman Hogg <hoggtruman@gmail.com>
2025-04-08 00:17:50 +0100

    Add RegexBasicsIsItASixBitUnsignedInteger

---
+++ 7 kyu/RegexpBasicsIsItASixBitUnsignedInteger.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/567e8dbb9b6f4da558000030
+
+namespace RegexpBasicsIsItASixBitUnsignedInteger;
+
+using System.Text.RegularExpressions;
+
+static class SimpleExtensions 
+{
+    public static bool SixBitNumber(this string str) 
+    {
+        return Regex.IsMatch(str, @"^([1-5]?\d|6[0-3])\z");
+    }
+}
