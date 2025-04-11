bf7f7a34cc99822600fba2f413cd33e001a98c71
Truman Hogg <hoggtruman@gmail.com>
2025-04-12 00:01:32 +0100

    Add ConsecutiveDigitsToFormSum

---
+++ 7 kyu/ConsecutiveDigitsToFormSum.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/5dae2599a8f7d90025d2f15f/train/csharp
+
+namespace ConsecutiveDigitsToFormSum;
+
+using System;
+
+public class Kata 
+{
+    public static bool ConsecutiveDucks(int n) 
+    {
+        return Math.Log2(n) % 1 != 0;
+    }
+}
