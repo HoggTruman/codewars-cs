4ef2fb38aa3f7d2a1159056a791681dca4712e2d
Truman Hogg <hoggtruman@gmail.com>
2025-04-11 20:32:55 +0100

    Add HowManyUrinalsAreFree

---
+++ 7 kyu/HowManyUrinalsAreFree.cs
@@ -0,0 +1,33 @@
+﻿// https://www.codewars.com/kata/5e2733f0e7432a000fb5ecc4
+
+namespace HowManyUrinalsAreFree;
+
+public static class FreeUrinals
+{
+    public static int GetFreeUrinals(string urinals)
+    {
+        int freeUrinals = 0;
+
+        for (int i = 1; i < urinals.Length - 1; ++i)
+        {
+            if (urinals[i] == '1' &&
+                (urinals[i - 1] == '1' || urinals[i + 1] == '1'))
+            {
+                return -1;
+            }
+        }
+
+        for (int i = 0; i < urinals.Length; ++i)
+        {
+            if (urinals[i] == '0' &&
+                (i - 1 < 0 || urinals[i - 1] == '0') &&
+                (i + 1 >= urinals.Length || urinals[i + 1] == '0'))
+            {
+                ++freeUrinals;
+                ++i;
+            }
+        }
+
+        return freeUrinals;
+    }
+}
