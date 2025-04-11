39bb703a7946c2f11b9e5357cc61545c4e3a17f0
Truman Hogg <hoggtruman@gmail.com>
2025-04-11 20:04:34 +0100

    Add NiceArray

---
+++ 7 kyu/NiceArray.cs
@@ -0,0 +1,28 @@
+﻿// https://www.codewars.com/kata/59b844528bcb7735560000a0
+
+namespace NiceArray;
+
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static bool IsNice(int[] arr)
+    {
+        if (arr.Length == 0)
+        {
+            return false;
+        }
+
+        HashSet<int> nums = new(arr);
+
+        foreach (int num in nums)
+        {
+            if (!nums.Contains(num + 1) && !nums.Contains(num - 1))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
