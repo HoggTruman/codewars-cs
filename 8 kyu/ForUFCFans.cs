e4532513e452188b87cfed77628957ef04eed43a
Truman Hogg <hoggtruman@gmail.com>
2025-04-15 19:40:56 +0100

    Add ForUFCFans

---
+++ 8 kyu/ForUFCFans.cs
@@ -0,0 +1,18 @@
+﻿// https://www.codewars.com/kata/582dafb611d576b745000b74
+
+namespace ForUFCFans;
+
+using System;
+
+public static class Kata
+{
+    public static string Quote(string fighter)
+    {
+        return fighter.ToLower() switch
+        {
+            "george saint pierre" => "I am not impressed by your performance.",
+            "conor mcgregor" => "I'd like to take this chance to apologize.. To absolutely NOBODY!",
+            _ => throw new ArgumentException()
+        };
+    }
+}
