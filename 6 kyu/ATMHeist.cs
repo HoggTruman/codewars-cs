ee453270b164ebe8161bb51acad670ce37680990
Truman Hogg <hoggtruman@gmail.com>
2025-04-16 22:19:52 +0100

    Add ATMHeist

---
+++ 6 kyu/ATMHeist.cs
@@ -0,0 +1,34 @@
+﻿// https://www.codewars.com/kata/5d8108a41e94580023bd6419
+
+namespace ATMHeist;
+
+public class Kata 
+{
+    public static int MaximumThrill(int[] arr) 
+    {
+        if (arr.Length == 0)
+        {
+            return 0;
+        }
+
+        int startIndex = 0;
+        for (int i = 1; i < arr.Length; ++i)
+        {
+            if (arr[i] > arr[startIndex] + i - startIndex)
+            {
+                startIndex = i;
+            }
+        }
+
+        int endIndex = startIndex;
+        for (int i = startIndex + 1; i < arr.Length; ++i)
+        {
+            if (arr[i] + i - endIndex > arr[endIndex])
+            {
+                endIndex = i;
+            }
+        }
+
+        return arr[startIndex] + arr[endIndex] + endIndex - startIndex;
+    }
+}
