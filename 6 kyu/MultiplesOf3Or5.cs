9dea99ad675872167b730b3aa7c2955816590885
Truman Hogg <hoggtruman@gmail.com>
2025-04-17 16:06:19 +0100

    Add MultiplesOf3Or5

---
+++ 6 kyu/MultiplesOf3Or5.cs
@@ -0,0 +1,21 @@
+﻿// https://www.codewars.com/kata/514b92a657cdc65150000006
+
+namespace MultiplesOf3Or5;
+
+public static class Kata
+{
+    public static int Solution(int value)
+    {
+        int sum = 0;
+
+        for (int k = 3; k < value; ++k)
+        {
+            if (k % 3 == 0 || k % 5 == 0)
+            {
+                sum += k;
+            }
+        }
+
+        return sum;
+    }
+}
