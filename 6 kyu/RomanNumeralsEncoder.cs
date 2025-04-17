ccef2c5c30399637ff8eaf36d09c127be06d9a74
Truman Hogg <hoggtruman@gmail.com>
2025-04-17 17:26:20 +0100

    Add RomanNumeralsEncoder

---
+++ 6 kyu/RomanNumeralsEncoder.cs
@@ -0,0 +1,44 @@
+﻿// https://www.codewars.com/kata/51b62bf6a9c58071c600001b
+
+namespace RomanNumeralsEncoder;
+
+using System.Collections.Generic;
+using System.Text;
+
+public class RomanConvert
+{
+    public static readonly Dictionary<string, int> SymbolValues = new()
+	{
+		["M"] = 1000,
+		["CM"] = 900,
+		["D"] = 500,
+		["CD"] = 400,
+		["C"] = 100,
+		["XC"] = 90,
+		["L"] = 50,
+		["XL"] = 40,
+		["X"] = 10,
+		["IX"] = 9,
+		["V"] = 5,
+		["IV"] = 4,
+		["I"] = 1
+	};
+
+	public static readonly List<string> Symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]; 
+
+	public static string Solution(int n)
+	{
+		StringBuilder sb = new();
+		foreach(string symbol in Symbols)
+		{
+			while (SymbolValues[symbol] <= n)
+			{
+				sb.Append(symbol);
+				n -= SymbolValues[symbol];
+			}
+		}
+
+		return sb.ToString();
+	}
+}
+
