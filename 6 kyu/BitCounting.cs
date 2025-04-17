6485338efeb6a563c5a58ea33837791ebfb03836
Truman Hogg <hoggtruman@gmail.com>
2025-04-17 16:06:02 +0100

    Add BitCounting

---
+++ 6 kyu/BitCounting.cs
@@ -0,0 +1,18 @@
+﻿// https://www.codewars.com/kata/526571aae218b8ee490006f4
+
+namespace BitCounting;
+
+public class Kata
+{
+    public static int CountBits(int n)
+    {
+        int count = 0;
+        while (n > 0)
+        {
+            count += n & 1;
+            n >>= 1;
+        }
+
+        return count;
+    }
+}
