ee949b75a21746e2241e74e4e2621e164b76a777
Truman Hogg <hoggtruman@gmail.com>
2025-04-18 15:26:47 +0100

    BuildTowerAdvanced

---
+++ 6 kyu/BuildTowerAdvanced.cs
@@ -0,0 +1,23 @@
+﻿// https://www.codewars.com/kata/57675f3dedc6f728ee000256
+
+namespace BuildTowerAdvanced;
+
+public class Kata
+{  
+    public static string[] TowerBuilder(int nFloors, int blockWidth, int blockHeight)
+    {
+        string[] building = new string[nFloors * blockHeight];
+
+        for (int i = 0; i < nFloors; ++i)
+        {
+            string padding = new(' ', blockWidth * (nFloors - i - 1));
+            string row =  padding + new string('*', blockWidth * (2 * i + 1)) + padding;
+            for (int h = 0; h < blockHeight; ++h)
+            {
+                building[i * blockHeight + h] = row;
+            }
+        }        
+
+        return building;
+    }
+}
