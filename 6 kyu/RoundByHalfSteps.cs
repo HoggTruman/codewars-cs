e4199862ddde42a64cb3b784d32aa54b048cefef
Truman Hogg <hoggtruman@gmail.com>
2025-04-18 15:44:01 +0100

    Add RoundByHalfSteps

---
+++ 6 kyu/RoundByHalfSteps.cs
@@ -0,0 +1,14 @@
+﻿// https://www.codewars.com/kata/51f1342c76b586046800002a
+
+namespace RoundByHalfSteps;
+
+using System;
+
+public class Kata
+{
+    public static double Solution(double n)
+    {
+        double x = Math.Abs(n % 0.5) < 0.25? (int)n: (int)n + Math.Sign(n) * 0.5;
+        return n % 1 < 0.5? x: Math.Sign(n) * 0.5;
+    }
+}
