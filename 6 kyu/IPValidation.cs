e1906077be93ff09caa4de3eb48cc28ad24b4219
Truman Hogg <hoggtruman@gmail.com>
2025-04-18 14:28:50 +0100

    Add IPValidation

---
+++ 6 kyu/IPValidation.cs
@@ -0,0 +1,35 @@
+﻿// https://www.codewars.com/kata/515decfd9dcfc23bb6000006
+
+namespace IPValidation;
+
+using System.Linq;
+
+class Kata
+{
+    public static bool IsValidIp(string ipAddress)
+    {
+        var parts = ipAddress.Split('.');
+        
+        if (parts.Length != 4 || parts.Any(x => x.Length == 0))
+        {
+            return false;
+        }
+
+        foreach(string part in parts)
+        {
+            if (part.Length > 1 && part.StartsWith('0') ||
+                part.Any(x => !char.IsDigit(x)))
+            {
+                return false;
+            }
+
+            int octet = int.Parse(part);
+            if (octet > 255 || octet < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
