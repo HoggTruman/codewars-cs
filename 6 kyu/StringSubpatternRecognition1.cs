091e97613c0aae8696af0c377974c3dc1cbe3f69
Truman Hogg <hoggtruman@gmail.com>
2025-04-22 18:36:33 +0100

    Add StringSubpatternRecognition

---
+++ 6 kyu/StringSubpatternRecognition1.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/5a49f074b3bfa89b4c00002b
+
+namespace StringSubpatternRecognition1;
+
+class Kata
+{
+    public static bool HasSubpattern(string str)
+    {
+        return (str + str).IndexOf(str, 1) != str.Length;
+    }
+}
