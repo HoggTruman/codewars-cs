037df442b05c1186ad77616842f8765c53773ea8
Truman Hogg <hoggtruman@gmail.com>
2025-04-25 17:35:22 +0100

    Add RectanglesIntoSquares

---
+++ 6 kyu/RectanglesIntoSquares.cs
@@ -0,0 +1,35 @@
+﻿// https://www.codewars.com/kata/55466989aeecab5aac00003e
+
+namespace RectanglesIntoSquares;
+
+using System.Collections.Generic;
+
+public class SqInRect 
+{	
+	public static List<int> sqInRect(int length, int width) 
+	{
+		if (length == width)
+		{
+			return null!;
+		}
+			
+		List<int> result = [];
+
+		while (length > 0 && width > 0)
+		{
+			if (width < length)
+			{
+				result.Add(width);
+				length -= width;
+			}
+			else
+			{
+				result.Add(length);
+				width -= length;
+			}
+		}
+		
+		return result;
+	}
+}
+
