065dbd84a34b6af0ee9daf5d08d2419d533a400c
Truman Hogg <hoggtruman@gmail.com>
2025-04-28 20:36:13 +0100

    Add NameArrayCapping

---
+++ 7 kyu/NameArrayCapping.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/5356ad2cbb858025d800111d
+
+namespace NameArrayCapping;
+
+using System.Linq;
+
+public static class Kata
+{
+    public static string[] CapMe(string[] strings)
+    {
+        return strings.Select(x => x[..1].ToUpper() + x[1..].ToLower()).ToArray();
+    }
+}
