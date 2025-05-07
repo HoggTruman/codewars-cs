971b8ed9ebb195e273533f7f3a9df19de308b83b
Truman Hogg <hoggtruman@gmail.com>
2025-05-07 20:41:54 +0100

    Add GroupedByCommas

---
+++ 6 kyu/GroupedByCommas.cs
@@ -0,0 +1,33 @@
+﻿// https://www.codewars.com/kata/5274e122fc75c0943d000148
+
+namespace GroupedByCommas;
+
+using System.Text;
+
+public static class Kata
+{
+    public static string GroupByCommas(int n)
+    {
+        StringBuilder sb = new();
+        string number = n.ToString();
+
+        if (number.Length <= 3)
+        {
+            return number;
+        }
+
+        int firstComma = (number.Length + 2) % 3;
+
+        for (int i = 0; i < number.Length; ++i)
+        {
+            sb.Append(number[i]);
+
+            if (i != number.Length - 1 && i % 3 == firstComma)
+            {
+                sb.Append(',');
+            }
+        }
+        
+        return sb.ToString();
+    }
+}
