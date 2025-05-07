2a29ce4ecdec526d77a3baacae406f5234419675
Truman Hogg <hoggtruman@gmail.com>
2025-05-07 19:03:43 +0100

    Add ChainMe

---
+++ 7 kyu/ChainMe.cs
@@ -0,0 +1,20 @@
+﻿// https://www.codewars.com/kata/54fb853b2c8785dd5e000957
+
+namespace ChainMe;
+
+using System;
+
+public static class Kata
+{
+    public static double Chain(double input, Func<double,double>[] fs)
+    {
+        double result = input;
+
+        foreach(var func in fs)
+        {
+            result = func(result);
+        }
+
+        return result;
+    }
+}
