e72f38289af25e22d11c940d2c6599111557a874
Truman Hogg <hoggtruman@gmail.com>
2025-05-08 18:40:24 +0100

    Add InvertingAHash

---
+++ 7 kyu/InvertingAHash.cs
@@ -0,0 +1,22 @@
+﻿// https://www.codewars.com/kata/5b5604e26dc79e6832000101
+
+namespace InvertingAHash;
+
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static Dictionary<TValue, TKey> InvertHash<TKey, TValue>(Dictionary<TKey, TValue> dictionary) 
+        where TKey : notnull
+        where TValue : notnull
+    {
+        Dictionary<TValue, TKey> result = [];
+
+        foreach(var kv in dictionary)
+        {
+            result[kv.Value] = kv.Key;
+        }
+
+        return result;
+    }
+}
