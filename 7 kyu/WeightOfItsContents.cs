d2247156ef5d003442a4a019eaaaadd8303f141d
Truman Hogg <hoggtruman@gmail.com>
2025-05-09 17:37:08 +0100

    Add WeightOfItsContents

---
+++ 7 kyu/WeightOfItsContents.cs
@@ -0,0 +1,16 @@
+﻿// https://www.codewars.com/kata/53921994d8f00b93df000bea
+
+namespace WeightOfItsContents;
+
+public static class SolutionClass
+{
+    public static int ContentWeight(int bottleWeight, string scaleString)
+    {
+        int scale = int.Parse(scaleString.Split()[0]);
+        bool hasLargerContents = scaleString[^6..] == "larger";
+
+        return hasLargerContents? 
+            bottleWeight / (scale + 1) * scale:
+            bottleWeight / (scale + 1);
+    }
+}
