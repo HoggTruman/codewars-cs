11c3e630291e97349e20b9a544515ef3847bbec6
Truman Hogg <hoggtruman@gmail.com>
2025-05-09 16:35:44 +0100

    Add CatsInHats

---
+++ 7 kyu/CatsInHats.cs
@@ -0,0 +1,23 @@
+﻿// https://www.codewars.com/kata/57b5907920b104772c00002a/train/csharp
+
+namespace CatsInHats;
+
+using System;
+
+public class Kata
+{
+    public static string Height(int n)
+    {
+        decimal totalHeight = 2_000_000;
+        decimal currentHeight = 2_000_000;
+
+        while (n > 0)
+        {
+            currentHeight /= 2.5m;
+            totalHeight += currentHeight;            
+            --n;
+        }
+
+        return Math.Round(totalHeight, 3).ToString("0.000");
+    }
+}
