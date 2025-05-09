08880281c491bc73e97bea65daacbe6d6c20f6ce
Truman Hogg <hoggtruman@gmail.com>
2025-05-09 17:16:55 +0100

    Add TheSpiralingBox

---
+++ 7 kyu/TheSpiralingBox.cs
@@ -0,0 +1,27 @@
+﻿// https://www.codewars.com/kata/63b84f54693cb10065687ae5
+
+namespace TheSpiralingBox;
+
+using System;
+
+public class SpiralingBox
+{
+    public static int[,] CreateBox(int cols, int rows)
+    {
+        int[,] box = new int[rows, cols];
+        int maxRow = (rows - 1) / 2;
+        int maxCol = (cols - 1) / 2;
+
+        for (int i = 0; i < rows; ++i)
+        {
+            int rowVal = i <= maxRow? i + 1: rows - i;
+            for (int j = 0; j < cols; ++j)
+            {                
+                int colVal = j <= maxCol? j + 1: cols - j;
+                box[i, j] = Math.Min(rowVal, colVal);
+            }
+        }
+
+        return box;
+    }
+}
