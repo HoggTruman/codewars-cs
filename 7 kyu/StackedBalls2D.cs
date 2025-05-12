57fa521284c7a3c537c7b8f9893f25ffd3286181
Truman Hogg <hoggtruman@gmail.com>
2025-05-12 20:04:24 +0100

    Add StackedBalls2D

---
+++ 7 kyu/StackedBalls2D.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/5bb804397274c772b40000ca
+
+namespace StackedBalls2D;
+
+using System;
+
+public class Dinglemouse
+{
+    public static double StackHeight2d(int layers)
+    {
+        return layers > 0? 1 + (layers - 1) * Math.Sqrt(3) / 2: 0;
+    }
+}
