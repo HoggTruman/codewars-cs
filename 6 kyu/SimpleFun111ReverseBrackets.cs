bd69b9d70bfca7d761949fb9714beb923d1dd76a
Truman Hogg <hoggtruman@gmail.com>
2025-05-13 21:35:36 +0100

    Add SimpleFun111ReverseBrackets

---
+++ 6 kyu/SimpleFun111ReverseBrackets.cs
@@ -0,0 +1,64 @@
+﻿// https://www.codewars.com/kata/589ace5eeef39faf49000061
+
+namespace SimpleFun111ReverseBrackets;
+
+using System;
+using System.Text;
+
+public class Kata
+{
+    public string ReverseParentheses(string s)
+    {
+        StringBuilder sb = new();
+
+        int i = 0;
+        while (i < s.Length)
+        {
+            if (s[i] == '(')
+            {
+                int closingBracketIndex = FindClosingBracket(s, i);
+                string reversed = Reverse(ReverseParentheses(s[(i + 1)..closingBracketIndex]));
+                sb.Append(reversed);
+                i = closingBracketIndex + 1;
+            }
+            else
+            {
+                sb.Append(s[i]);
+                ++i;
+            }
+        }
+          
+        return sb.ToString();
+    }
+
+    public static int FindClosingBracket(string s, int openBracketIndex)
+    {
+        int openBrackets = 1;
+
+        for (int i = openBracketIndex + 1; i < s.Length; ++i)
+        {
+            if (s[i] == '(')
+            {
+                ++openBrackets;
+            }
+            else if (s[i] == ')')
+            {
+                --openBrackets;
+
+                if (openBrackets == 0)
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static string Reverse(string s)
+    {
+        char[] arr = s.ToCharArray();
+        Array.Reverse(arr);
+        return new(arr);
+    }
+}
