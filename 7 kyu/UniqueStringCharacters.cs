218c15ed58cb05ca60df1b161ff3e1760f1c7817
Truman Hogg <hoggtruman@gmail.com>
2025-05-14 19:41:46 +0100

    Add UniqueStringCharacters

---
+++ 7 kyu/UniqueStringCharacters.cs
@@ -0,0 +1,16 @@
+﻿// https://www.codewars.com/kata/5a262cfb8f27f217f700000b
+
+namespace UniqueStringCharacters;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static string Solve(string a, string b)
+    {
+        HashSet<char> aChars = [.. a];
+        HashSet<char> bChars = [.. b];        
+        return new string([..a.Where(x => !bChars.Contains(x)), ..b.Where(x => !aChars.Contains(x))]);
+    }
+}
