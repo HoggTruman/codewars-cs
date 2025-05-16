734770f9e37cdaa4dd9b9bc41684ab987bb9c00c
Truman Hogg <hoggtruman@gmail.com>
2025-05-16 18:34:56 +0100

    Add Collatz

---
+++ 6 kyu/Collatz.cs
@@ -0,0 +1,24 @@
+﻿// https://www.codewars.com/kata/5286b2e162056fd0cb000c20
+
+namespace Collatz;
+
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static string Collatz(int n)
+    {
+        List<int> terms = [n];
+
+        while (n != 1)
+        {
+            n = n % 2 == 0?
+                n / 2:
+                3 * n + 1;
+
+            terms.Add(n);
+        }
+
+        return string.Join("->", terms);
+    }
+}
