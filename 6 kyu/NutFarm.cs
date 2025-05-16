5f0ed026114e23446976a9cf555f3a6c5e0265f4
Truman Hogg <hoggtruman@gmail.com>
2025-05-16 17:07:21 +0100

    Add NutFarm

---
+++ 6 kyu/NutFarm.cs
@@ -0,0 +1,43 @@
+﻿// https://www.codewars.com/kata/59884371d1d8d3d9270000a5
+
+namespace NutFarm;
+
+public class Dinglemouse
+{
+    public static int[] ShakeTree(string[] tree)
+    {
+        string nuts = tree[0];
+        int[] result = new int[nuts.Length];
+
+        for (int i = 0; i < nuts.Length; ++i)
+        {
+            if (nuts[i] != 'o')
+            {
+                continue;
+            }
+
+            int row = 1;
+            int col = i;
+            while (row < tree.Length && tree[row][col] != '_')
+            {
+                if (tree[row][col] == '\\')
+                {
+                    ++col;
+                }
+                else if (tree[row][col] == '/')
+                {
+                    --col;
+                }
+
+                ++row;
+            }
+
+            if (row == tree.Length)
+            {
+                ++result[col];
+            }
+        }
+
+        return result;
+    }
+}
