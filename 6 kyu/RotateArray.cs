b642cffa9a105b7661a5d6d794d3ce874b50125a
Truman Hogg <hoggtruman@gmail.com>
2025-05-16 18:42:11 +0100

    Add RotateArray

---
+++ 6 kyu/RotateArray.cs
@@ -0,0 +1,14 @@
+﻿// https://www.codewars.com/kata/54f8b0c7a58bce9db6000dc4
+
+namespace RotateArray;
+
+public class Kata
+{
+    public static object[] Rotate(object[] array, int n)
+    {
+        n %= array.Length;
+        return n >= 0?
+            [..array[^n..], ..array[..^n]]:
+            [..array[-n..], ..array[..-n]];
+    }
+}
