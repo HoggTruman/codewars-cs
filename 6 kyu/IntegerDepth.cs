bef80ce6f3c6678a605d0774fa930b711d51bedd
Truman Hogg <hoggtruman@gmail.com>
2025-05-16 16:43:27 +0100

    Add IntegerDepth

---
+++ 6 kyu/IntegerDepth.cs
@@ -0,0 +1,30 @@
+﻿// https://www.codewars.com/kata/59b401e24f98a813f9000026
+
+namespace IntegerDepth;
+
+using System.Collections.Generic;
+
+public class Kata 
+{
+    public static int ComputeDepth(int n)
+    {
+        HashSet<int> discovered = [];
+
+        int term = n;
+        int count = 0;
+
+        while (discovered.Count < 10)
+        {
+            while (term > 0)
+            {
+                discovered.Add(term % 10);
+                term /= 10;
+            }
+
+            ++count;
+            term = (count + 1) * n;            
+        }
+
+        return count;
+    }
+}
