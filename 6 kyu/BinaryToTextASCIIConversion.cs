b3567b76a09c6acea0e0aa377b0fb39a75fc971f
Truman Hogg <hoggtruman@gmail.com>
2025-05-20 19:05:52 +0100

    Add BinaryToTextASCIIConversion

---
+++ 6 kyu/BinaryToTextASCIIConversion.cs
@@ -0,0 +1,21 @@
+﻿// https://www.codewars.com/kata/5583d268479559400d000064
+
+namespace BinaryToTextASCIIConversion;
+
+using System;
+using System.Text;
+
+public static class Kata
+{
+    public static string BinaryToString(string binary)
+    {
+        var sb = new StringBuilder();
+      
+        for (int i = 0; i < binary.Length; i += 8)
+        {
+            sb.Append((char)Convert.ToByte(binary[i..(i+8)], 2));
+        }
+      
+        return sb.ToString();
+    }
+}
