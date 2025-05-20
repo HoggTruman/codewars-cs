2c863b5d6d7f293fe3b1661a39f36f7a016e32f0
Truman Hogg <hoggtruman@gmail.com>
2025-05-20 20:49:14 +0100

    Add BinarySXORe

---
+++ 7 kyu/BinarySXORe.cs
@@ -0,0 +1,22 @@
+﻿// https://www.codewars.com/kata/56d3e702fc231fdf72001779
+
+namespace BinarySXORe;
+
+using System;
+using System.Numerics;
+
+public class BinarySxore
+{
+    public static BigInteger Sxore (BigInteger n)
+    {
+        int mod = (int)(n % 4);
+        return mod switch
+        {
+            0 => n,
+            1 => 1,
+            2 => n + 1,
+            3 => 0,
+            _ => throw new Exception("Something went wrong.")
+        };
+    }
+}
