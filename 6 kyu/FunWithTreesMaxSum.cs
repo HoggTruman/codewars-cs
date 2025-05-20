4f5dfc95f343db1fb260ae50ddb430727ad95279
Truman Hogg <hoggtruman@gmail.com>
2025-05-20 21:15:59 +0100

    Add FunWithTreesMaxSum

---
+++ 6 kyu/FunWithTreesMaxSum.cs
@@ -0,0 +1,35 @@
+﻿// https://www.codewars.com/kata/57e5279b7cf1aea5cf000359
+
+namespace FunWithTreesMaxSum;
+
+using System;
+
+class TreeNode
+{
+    public TreeNode? left;
+    public TreeNode? right;
+    public int value;
+}
+
+class Solution
+{
+    public static int MaxSum(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        if (root.left != null && root.right == null)
+        {
+            return root.value + MaxSum(root.left);
+        }
+
+        if (root.right != null && root.left == null)
+        {
+            return root.value + MaxSum(root.right);
+        }
+
+        return root.value + Math.Max(MaxSum(root.left), MaxSum(root.right));
+    }
+}
