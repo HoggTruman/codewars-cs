202004b8ce0181eeb58c7a5937eafadce333510a
Truman Hogg <hoggtruman@gmail.com>
2025-05-20 19:50:41 +0100

    Add BinaryScORe

---
+++ 7 kyu/BinaryScORe.cs
@@ -0,0 +1,20 @@
+﻿// https://www.codewars.com/kata/56cafdabc8cfcc3ad4000a2b
+
+namespace BinaryScORe;
+
+using System.Numerics;
+
+public class BinaryScore
+{
+    public static BigInteger Score(BigInteger n)
+    {
+        BigInteger score = 0;
+
+        for (BigInteger k = 1; k <= n; k *= 2)
+        {
+            score += k;
+        }
+
+        return score;
+    }
+}
