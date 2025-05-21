6570ab3fa8cb3d4d23c444aecca61375e91a7132
Truman Hogg <hoggtruman@gmail.com>
2025-05-21 17:38:20 +0100

    Add ValidPhoneNumber

---
+++ 6 kyu/ValidPhoneNumber.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/525f47c79f2f25a4db000025
+
+namespace ValidPhoneNumber;
+
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static bool ValidPhoneNumber(string phoneNumber)
+    {
+        return Regex.IsMatch(phoneNumber, @"^\(\d{3}\) \d{3}-\d{4}$");
+    }
+}
