ff7475d7f1f261c534744e478ee2217da842f9b3
Truman Hogg <hoggtruman@gmail.com>
2025-05-21 17:24:28 +0100

    Add FunWithTreesArrayToTree

---
+++ 5 kyu/FunWithTreesArrayToTree.cs
@@ -0,0 +1,54 @@
+﻿// https://www.codewars.com/kata/57e5a6a67fbcc9ba900021cd
+
+namespace FunWithTreesArrayToTree;
+
+class TreeNode 
+{
+    public TreeNode? left;
+    public TreeNode? right;
+    public int value;
+    
+    public TreeNode(int value, TreeNode left, TreeNode right)
+    {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+    
+    public TreeNode(int value) 
+    {
+        this.value = value;
+    }
+}
+
+class Solution
+{
+    public static TreeNode? ArrayToTree(int[] array)
+    {
+        TreeNode[] nodes = new TreeNode[array.Length];
+
+        for (int i = 0; i < array.Length; ++i)
+        {
+            nodes[i] = new TreeNode(array[i]);
+            if (i != 0)
+            {
+                TreeNode parent = nodes[GetParent(i)];
+                if (i % 2 == 0)
+                {
+                    parent.right = nodes[i];
+                }
+                else
+                {
+                    parent.left = nodes[i];
+                }
+            }
+        }
+
+        return nodes.Length > 0? nodes[0]: null;
+    }
+
+    public static int GetParent(int i)
+    {
+        return (i - 1) / 2;
+    }
+}
