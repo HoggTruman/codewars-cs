97842232ed789b8750f836ff6d50db3bd6e6b79f
Truman Hogg <hoggtruman@gmail.com>
2025-05-21 20:09:00 +0100

    Add PatternCraftState

---
+++ 6 kyu/PatternCraftState.cs
@@ -0,0 +1,36 @@
+﻿// https://www.codewars.com/kata/5682e72eb7354b2f39000021
+
+namespace PatternCraftState;
+
+public interface IUnit
+{
+    IUnitState State { get; set; }
+    bool CanMove { get; }
+    int Damage { get; }
+}
+
+public interface IUnitState
+{
+    bool CanMove { get; set; }
+    int Damage { get; set; }
+}
+
+public class SiegeState : IUnitState
+{
+    public bool CanMove { get; set; } = false;
+    public int Damage { get; set; } = 20;
+}
+
+public class TankState : IUnitState
+{
+    public bool CanMove { get; set; } = true;
+    public int Damage { get; set; } = 5;
+}
+
+public class Tank : IUnit
+{
+    public IUnitState State { get; set; } = new TankState();
+
+    public bool CanMove => State.CanMove;
+    public int Damage => State.Damage;
+}
