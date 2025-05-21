f884b646ccab983d6a2082612193fc1718c514d5
Truman Hogg <hoggtruman@gmail.com>
2025-05-21 19:42:18 +0100

    Add AmISafeToDrive

---
+++ 6 kyu/AmISafeToDrive.cs
@@ -0,0 +1,33 @@
+﻿// https://www.codewars.com/kata/58ce88427e6c3f41c2000087
+
+namespace AmISafeToDrive;
+
+using System;
+using System.Collections.Generic;
+
+class Kata
+{
+    public Dictionary<double, bool> drive (double[,] drinks, string finished, string drive_time)
+    {
+        double bloodUnits = 0;
+        for (int i = 0; i < drinks.GetLength(0); ++i)
+        {
+            bloodUnits += drinks[i, 0] * drinks[i, 1] / 1000;
+        }
+
+        double finishedHours = ToHours(finished);
+        double startDriveHours = ToHours(drive_time);
+        double timeBetween = startDriveHours < finishedHours? 
+            24 + startDriveHours - finishedHours: 
+            startDriveHours - finishedHours;
+
+        return new() { [Math.Round(bloodUnits, 2)] = timeBetween > bloodUnits };
+    }
+
+    public static double ToHours(string time)
+    {
+        double hours = double.Parse(time[..2]);
+        double minutes = double.Parse(time[3..]);
+        return hours + minutes / 60;
+    }
+}
