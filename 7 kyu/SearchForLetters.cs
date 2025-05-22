58a7fba31be92901c1c89caf52bedb100ea26a11
Truman Hogg <hoggtruman@gmail.com>
2025-05-22 15:53:07 +0100

    Add SearchForLetters

---
+++ 7 kyu/SearchForLetters.cs
@@ -0,0 +1,23 @@
+﻿// https://www.codewars.com/kata/52dbae61ca039685460001ae
+
+namespace SearchForLetters;
+
+using System.Linq;
+
+public class Kata
+{
+    public static string Change(string input)
+    {
+        char[] isPresent = Enumerable.Repeat('0', 26).ToArray();
+
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c))
+            {
+                isPresent[char.ToLower(c) - 'a'] = '1';
+            }
+        }
+
+        return string.Join("", isPresent);
+    }
+}
