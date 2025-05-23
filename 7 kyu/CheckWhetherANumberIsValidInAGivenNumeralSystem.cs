e5ed0125aa0d99563ec82c73926fb6a9c5ab836d
Truman Hogg <hoggtruman@gmail.com>
2025-05-23 20:22:24 +0100

    Add CheckWhetherANumberIsValidInAGivenNumeralSystem

---
+++ 7 kyu/CheckWhetherANumberIsValidInAGivenNumeralSystem.cs
@@ -0,0 +1,17 @@
+﻿// https://www.codewars.com/kata/67757660c552a3a7ef9aaceb
+
+namespace CheckWhetherANumberIsValidInAGivenNumeralSystem;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class SolutionClass
+{
+    private readonly static char[] characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+    public static bool ValidateBase(string num, int base_)
+    {
+        HashSet<char> validCharacters = new(characters[..base_]);
+        return num.All(validCharacters.Contains);
+    }
+}
