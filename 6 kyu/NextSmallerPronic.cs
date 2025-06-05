1a1c1e1ea9e08b84ff01ccbf67f5330d3456e522
Truman Hogg <hoggtruman@gmail.com>
2025-06-05 12:55:24 +0100

    Add NextSmallerPronic

---
+++ 6 kyu/NextSmallerPronic.cs
@@ -0,0 +1,14 @@
+﻿// https://www.codewars.com/kata/5a90f6d457c5624ecc000012
+
+namespace NextSmallerPronic;
+
+using System;
+
+public static class Kata
+{
+    public static ulong NextSmallerPronic(ulong x)
+    {
+        ulong sqrt = Convert.ToUInt64(Math.Sqrt(x));
+        return sqrt * (sqrt - 1);
+    }
+}
