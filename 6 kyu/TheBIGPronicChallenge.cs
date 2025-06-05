75b39c2dcbe37dcce3d81257f243cc31366a9b15
Truman Hogg <hoggtruman@gmail.com>
2025-06-05 13:01:51 +0100

    Add TheBIGPronicChallenge

---
+++ 6 kyu/TheBIGPronicChallenge.cs
@@ -0,0 +1,19 @@
+﻿// https://www.codewars.com/kata/55f160df93f61bb26b00002a
+
+namespace TheBIGPronicChallenge;
+
+using System;
+
+public class Kata
+{
+    public static bool IsPronic(long n)
+    {
+        if (n < 0)
+        {
+            return false;
+        }
+
+        long sqrt = (long)Math.Sqrt(n);
+        return sqrt * (sqrt + 1) == n;
+    }
+}
