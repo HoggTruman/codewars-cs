63548684f60fa57c40b86fc02b359e5b6e00942c
Truman Hogg <hoggtruman@gmail.com>
2025-06-19 23:02:42 +0100

    Add FoldAnArary

---
+++ 6 kyu/FoldAnArray.cs
@@ -0,0 +1,28 @@
+﻿// https://www.codewars.com/kata/57ea70aa5500adfe8a000110
+
+namespace FoldAnArray;
+
+public class Kata
+{
+    public static int[] FoldArray(int[] array, int runs)
+    {
+        int[] folded = array;
+        for (int run = 1; run <= runs; ++run)
+        {
+            int[] next = new int[(folded.Length + 1) / 2];
+            for (int i = 0; i < next.Length; ++i)
+            {
+                next[i] = folded[i] + folded[folded.Length - i - 1];
+            }
+
+            if (folded.Length % 2 == 1)
+            {
+                next[next.Length - 1] = folded[next.Length - 1];
+            }
+
+            folded = next;
+        }
+
+        return folded;
+    }
+}
