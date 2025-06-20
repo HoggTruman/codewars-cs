3dad1b9274b37a6a706a54b4292b707618cb89e2
Truman Hogg <hoggtruman@gmail.com>
2025-06-20 21:07:45 +0100

    Add PairOfGloves

---
+++ 6 kyu/PairOfGloves.cs
@@ -0,0 +1,20 @@
+﻿// https://www.codewars.com/kata/58235a167a8cb37e1a0000db
+
+namespace PairOfGloves;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int NumberOfPairs(string[] gloves)
+    {
+        Dictionary<string, int> counts = [];
+        foreach (string color in gloves)
+        {
+            counts[color] = counts.TryGetValue(color, out int count)? count + 1: 1;
+        }
+
+        return counts.Aggregate(0, (acc, x) => acc + x.Value / 2);
+    }
+}
