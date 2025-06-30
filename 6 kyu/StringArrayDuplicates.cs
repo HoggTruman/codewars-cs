b4e4d00feb587a0a2ef49aa95d6007bea54fff3d
Truman Hogg <hoggtruman@gmail.com>
2025-06-30 20:48:01 +0100

    Add StringArrayDuplicates

---
+++ 6 kyu/StringArrayDuplicates.cs
@@ -0,0 +1,28 @@
+﻿// https://www.codewars.com/kata/59f08f89a5e129c543000069
+
+namespace StringArrayDuplicates;
+
+using System.Linq;
+using System.Text;
+
+public class Solution
+{
+    public static string[] dup(string[] arr)
+    {
+        return arr.Select(RemoveConsecutiveDuplicates).ToArray();
+    }
+
+    private static string RemoveConsecutiveDuplicates(string s)
+    {
+        StringBuilder sb = new();
+        foreach(char c in s)
+        {
+            if (sb.Length == 0 || c != sb[^1])
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
