e17edea6e223b48859b7d17cedf95361e1fd89ba
Truman Hogg <hoggtruman@gmail.com>
2025-07-01 20:16:17 +0100

    Add ArrayDeepCount

---
+++ 6 kyu/ArrayDeepCount.cs
@@ -0,0 +1,17 @@
+﻿// https://www.codewars.com/kata/596f72bbe7cd7296d1000029
+
+namespace ArrayDeepCount;   
+
+public class Kata
+{
+    public static int DeepCount(object a)
+    {
+        int count = 0;
+        foreach (object x in (object[])a)
+        {
+            count += x is object[]? 1 + DeepCount(x): 1;     
+        }
+
+        return count;
+    }
+}
