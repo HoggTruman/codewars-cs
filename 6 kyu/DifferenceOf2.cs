4242da0dc496fae4def5b2b2381f6b4688002d16
Truman Hogg <hoggtruman@gmail.com>
2025-07-01 19:45:35 +0100

    Add DifferenceOf2

---
+++ 6 kyu/DifferenceOf2.cs
@@ -0,0 +1,24 @@
+﻿// https://www.codewars.com/kata/5340298112fa30e786000688
+
+namespace DifferenceOf2;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static (int, int)[] TwosDifference(int[] array)
+    {
+        List<(int, int)> result = [];
+        HashSet<int> numbers = [.. array];
+
+        foreach (int k in array.Order())
+        {
+            if (numbers.Contains(k + 2))
+            {
+                result.Add((k, k + 2));
+            }
+        }
+        return [.. result];
+    }
+}
