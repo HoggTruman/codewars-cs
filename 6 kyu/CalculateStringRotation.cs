93d02fece1f7b524659875c9adb66e202545d41a
Truman Hogg <hoggtruman@gmail.com>
2025-07-02 16:57:20 +0100

    Add CalculateStringRotation

---
+++ 6 kyu/CalculateStringRotation.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/5596f6e9529e9ab6fb000014
+
+namespace CalculateStringRotation;
+
+public class CalculateStringRotation
+{
+    public static int ShiftedDiff(string first, string second)
+    {
+        return first.Length == second.Length? (second + second).IndexOf(first): -1;
+    }
+}
