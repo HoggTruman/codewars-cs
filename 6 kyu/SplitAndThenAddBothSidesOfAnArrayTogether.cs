6a3a3e1d69f253c2daef4705e4ae3bb39207371a
Truman Hogg <hoggtruman@gmail.com>
2025-07-07 20:14:50 +0100

    Add SplitAndThenAddBothSidesOfAnArrayTogether

---
+++ 6 kyu/SplitAndThenAddBothSidesOfAnArrayTogether.cs
@@ -0,0 +1,29 @@
+﻿// https://www.codewars.com/kata/5946a0a64a2c5b596500019a
+
+namespace SplitAndThenAddBothSidesOfAnArrayTogether;
+
+
+public class Kata 
+{
+    public static int[] SplitAndAdd(int[] numbers, int n) 
+    {
+        int[] result = [..numbers];        
+        
+        for (int k = 1; k <= n; ++k)
+        {
+            if (result.Length == 1)
+            {
+                return result;
+            }
+
+            for (int i = 0; i < result.Length / 2; ++i)
+            {
+                result[result.Length - i - 1] += result[result.Length / 2 - i - 1];
+            }
+
+            result = result[(result.Length / 2)..];
+        }
+
+        return result;
+    }
+}
