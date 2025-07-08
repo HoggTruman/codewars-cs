7a844985a04b4e773497a09434da940220617145
Truman Hogg <hoggtruman@gmail.com>
2025-07-08 18:05:32 +0100

    Add ThueMorseSequence

---
+++ 6 kyu/ThueMorseSequence.cs
@@ -0,0 +1,19 @@
+﻿// https://www.codewars.com/kata/591aa1752afcb02fa300002a
+
+namespace ThueMorseSequence;
+
+using System.Linq;
+
+public class Kata
+{
+    public static string ThueMorse(int n) 
+    {
+        string sequence = "0";
+        while (sequence.Length < n)
+        {
+            sequence += string.Join("", sequence.Select(x => x == '0'? '1': '0'));
+        }
+
+        return sequence[..n];
+    }
+}
