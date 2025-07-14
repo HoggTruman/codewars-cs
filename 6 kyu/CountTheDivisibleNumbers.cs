82637f429c9692b303acc302a3eb7a29868a6e2a
Truman Hogg <hoggtruman@gmail.com>
2025-07-14 21:28:38 +0100

    Add CountTheDivisibleNumbers

---
+++ 6 kyu/CountTheDivisibleNumbers.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/55a5c82cd8e9baa49000004c
+
+namespace CountTheDivisibleNumbers;
+
+public class Kata
+{
+    public static long DivisibleCount(long x, long y, long k)
+    {
+        return x % k == 0? y / k - x / k + 1: y / k - x / k;
+    }
+}
