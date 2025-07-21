d45430ae46b93d4af37592a80ba6a600fb0121a3
Truman Hogg <hoggtruman@gmail.com>
2025-07-21 17:48:53 +0100

    Add SchrodingersBoolean

---
+++ 6 kyu/SchrodingersBoolean.cs
@@ -0,0 +1,28 @@
+﻿// https://www.codewars.com/kata/5a5f9f80f5dc3f942b002309
+
+namespace SchrodingersBoolean;
+
+using System;
+
+public class AlwaysTrue
+{
+    public static bool operator ==(AlwaysTrue a, bool b)
+    {
+        if (a == null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static bool operator !=(AlwaysTrue a, bool b)
+    {
+        return !(a == b);
+    }
+}
+
+public class Kata
+{
+    public static AlwaysTrue omnibool = new();
+}
