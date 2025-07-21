0eb7bdac6f4aec026ef42af19a150f203d81d385
Truman Hogg <hoggtruman@gmail.com>
2025-07-21 16:48:02 +0100

    Add FindTheMine

---
+++ 6 kyu/FindTheMine.cs
@@ -0,0 +1,24 @@
+﻿// https://www.codewars.com/kata/528d9adf0e03778b9e00067e
+
+namespace FindTheMine;
+
+using System;
+
+public class Kata
+{
+    public static Tuple<int, int> MineLocation(int[,] field)
+    {
+        for (int i = 0; i < field.GetLength(0); ++i)
+        {
+            for (int j = 0; j < field.GetLength(1); ++j)
+            {
+                if (field[i, j] == 1)
+                {
+                    return new(i, j);
+                }
+            }
+        }
+
+        throw new InvalidOperationException("No Mine Present!");
+    }
+}
