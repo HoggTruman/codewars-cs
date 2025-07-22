b59ab2034fe7ee1ac9d7b6a3db415be2e0803c34
Truman Hogg <hoggtruman@gmail.com>
2025-07-22 19:45:24 +0100

    Add ArrayIntersectAll

---
+++ 6 kyu/ArrayIntersectAll.cs
@@ -0,0 +1,26 @@
+﻿// https://www.codewars.com/kata/52a4e42ce950ed50da000748
+
+namespace ArrayIntersectAll;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static string[] Intersect(params string[][] arrays)
+    {
+        if (arrays.Length == 0)
+        {
+            return [];
+        }
+
+        HashSet<string> elements = new(arrays[0]);
+
+        for (int i = 1; i < arrays.Length; ++i)
+        {
+            elements.IntersectWith(arrays[i]);
+        }
+
+        return elements.ToArray();
+    }
+}
