816eee829782098989c3b3ea94ba51fa72934c31
Truman Hogg <hoggtruman@gmail.com>
2025-07-23 17:56:59 +0100

    Add WaveSorting

---
+++ 6 kyu/WaveSorting.cs
@@ -0,0 +1,17 @@
+﻿// https://www.codewars.com/kata/596f28fd9be8ebe6ec0000c1
+
+namespace WaveSorting;
+
+using System;
+
+public class Kata
+{
+    public static void WaveSort(int[] arr)
+    {
+        Array.Sort(arr);
+        for (int i = 1; i < arr.Length; i += 2)
+        {
+            (arr[i], arr[i - 1]) = (arr[i - 1], arr[i]);
+        }
+    }
+}
