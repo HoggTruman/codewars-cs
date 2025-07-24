fd13947f82c6f701c008a0c167746fa83ac468a8
Truman Hogg <hoggtruman@gmail.com>
2025-07-24 19:35:11 +0100

    Add SimpleFun19CountBlackCells

---
+++ 6 kyu/SimpleFun19CountBlackCells.cs
@@ -0,0 +1,16 @@
+﻿// https://www.codewars.com/kata/588475d575431d0a0e000023
+
+namespace SimpleFun19CountBlackCells;
+
+public class Kata
+{
+    public int CountBlackCells(int h, int w)
+    {
+        return h + w + GCD(h, w) - 2;         
+    }
+
+    private static int GCD(int a, int b)
+    {
+        return b == 0 ? a : GCD(b, a % b);    
+    }
+}
