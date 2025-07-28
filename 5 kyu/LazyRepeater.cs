03c4f9527364158e72515630c3044d04dfdf381a
Truman Hogg <hoggtruman@gmail.com>
2025-07-28 17:41:41 +0100

    Add LazyRepeater

---
+++ 5 kyu/LazyRepeater.cs
@@ -0,0 +1,14 @@
+﻿// https://www.codewars.com/kata/51fc3beb41ecc97ee20000c3/csharp
+
+namespace LazyRepeater;
+
+using System;
+
+public class Kata
+{
+    public static Func<char> MakeLooper(string str)
+    {
+        int call = -1;
+        return () => str[++call % str.Length];
+    }
+}
