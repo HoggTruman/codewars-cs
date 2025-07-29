356484b1e4e06d3ca9c7adc590b656ff9bd6a002
Truman Hogg <hoggtruman@gmail.com>
2025-07-29 19:46:41 +0100

    Add ClockyMcClockFace

---
+++ 6 kyu/ClockyMcClockFace.cs
@@ -0,0 +1,14 @@
+﻿// https://www.codewars.com/kata/59752e1f064d1261cb0000ec
+
+namespace ClockyMcClockFace;
+
+public class Dinglemouse
+{
+    public static string WhatTimeIsIt(double angle)
+    {
+        int totalMinutes = (int)(angle * 2);
+        int hours = totalMinutes >= 60? totalMinutes / 60: 12;
+        int minutes = totalMinutes % 60;
+        return hours.ToString("D2") + ":" + minutes.ToString("D2");
+    }
+}
