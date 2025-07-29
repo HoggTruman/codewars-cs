2f61983b7cf263a59c9aa9964c368f4e2e9a9930
Truman Hogg <hoggtruman@gmail.com>
2025-07-29 18:50:20 +0100

    Add MatrixAddition

---
+++ 6 kyu/MatrixAddition.cs
@@ -0,0 +1,23 @@
+﻿// https://www.codewars.com/kata/526233aefd4764272800036f
+
+namespace MatrixAddition;
+
+public class Kata
+{
+    public static int[][] MatrixAddition(int[][] a, int[][] b)
+    {
+        int n = a.Length;
+        int[][] result = new int[n][];
+
+        for (int i = 0; i < n; ++i)
+        {
+            result[i] = new int[n];
+            for (int j = 0; j < n; ++j)
+            {
+                result[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        return result;
+    }
+}
