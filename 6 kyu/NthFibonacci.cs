1ce5f924dbf6eaac2b77cd97f5c6e60b6451fb32
Truman Hogg <hoggtruman@gmail.com>
2025-07-30 18:16:33 +0100

    Add NthFibonacci

---
+++ 6 kyu/NthFibonacci.cs
@@ -0,0 +1,20 @@
+﻿// https://www.codewars.com/kata/522551eee9abb932420004a0
+
+namespace NthFibonacci;
+
+using System.Collections.Generic;
+
+public class Fibonacci
+{
+    public int NthFib(int n)
+    {
+        List<int> sequence = [0, 1];
+
+        while (sequence.Count < n)
+        {
+            sequence.Add(sequence[^1] + sequence[^2]);
+        }
+
+        return sequence[n - 1];
+    }
+}
