a5e71ccc1b2560ed86355c74102c5283e9dc09f8
Truman Hogg <hoggtruman@gmail.com>
2025-07-31 17:33:12 +0100

    Add SortingByBits

---
+++ 6 kyu/SortingByBits.cs
@@ -0,0 +1,29 @@
+﻿// https://www.codewars.com/kata/59fa8e2646d8433ee200003f/csharp
+
+namespace SortingByBits;
+
+using System.Linq;
+
+public class Kata
+{
+    public static int[] SortByBit(int[] array)
+    {
+        return array.OrderBy(CountOnBits).ThenBy(x => x).ToArray();
+    }
+
+    private static int CountOnBits(int n)
+    {
+        int count = 0;
+        while (n > 0)
+        {
+            if (n % 2 == 1)
+            {
+                ++count;
+            }
+
+            n >>= 1;            
+        }
+
+        return count;
+    }
+}
