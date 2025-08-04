121e8b692dc9f1536f2c60bd60175d3c7c1d4b9a
Truman Hogg <hoggtruman@gmail.com>
2025-08-04 20:44:08 +0100

    Add LoopDetector

---
+++ 7 kyu/LoopDetector.cs
@@ -0,0 +1,27 @@
+﻿// https://www.codewars.com/kata/68851563123e161332d2a84b
+
+namespace LoopDetector;
+
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static bool HasLoop(int[] arr)
+    {
+        HashSet<int> visited = [];
+        int i = 0;
+
+        while (i < arr.Length)
+        {
+            if (visited.Contains(i))
+            {
+                return true;
+            }
+
+            visited.Add(i);
+            i = arr[i];
+        }
+
+        return false;
+    }
+}
