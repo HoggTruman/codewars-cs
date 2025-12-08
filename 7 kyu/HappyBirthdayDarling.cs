5840b8f7853cec093eddb972ff8b44efb969e6f5
Truman Hogg <hoggtruman@gmail.com>
2025-12-08 20:09:21 +0000

    Add HappyBirthdayDarling

---
+++ 7 kyu/HappyBirthdayDarling.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/5e96332d18ac870032eb735f
+
+namespace HappyBirthdayDarling;
+
+public class Kata
+{
+    public static string WomensAge(int n)
+    {
+        return n % 2 == 0? 
+            $"{n}? That's just 20, in base {n / 2}!":
+            $"{n}? That's just 21, in base {n / 2}!";
+    }
+}
