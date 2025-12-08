37cac1478916c63ddaeee2ef1603afd34f682d31
Truman Hogg <hoggtruman@gmail.com>
2025-12-08 20:08:25 +0000

    Add GivenAnArrayOfNumbers

---
+++ 7 kyu/GivenAnArrayOfNumbersWhichArePerfectSquares.cs
@@ -0,0 +1,24 @@
+﻿// https://www.codewars.com/kata/53227465e4d01b5d5200011e
+
+namespace GivenAnArrayOfNumbersWhichArePerfectSquares;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata 
+{
+    public static int[] GetSquares(int[] array)
+    {
+        HashSet<int> perfectSquares = [];
+        foreach (int k in array)
+        {
+            if (k >= 0 && Math.Sqrt(k) % 1 == 0)
+            {
+                perfectSquares.Add(k);
+            }
+        }
+
+        return perfectSquares.Order().ToArray();
+    }
+}
