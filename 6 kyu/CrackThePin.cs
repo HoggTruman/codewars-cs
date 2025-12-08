dff5694640bb5f636bc4b34e5c946ee38b704159
Truman Hogg <hoggtruman@gmail.com>
2025-12-08 21:52:42 +0000

    Add CrackThePin

---
+++ 6 kyu/CrackThePin.cs
@@ -0,0 +1,29 @@
+﻿// https://www.codewars.com/kata/5efae11e2d12df00331f91a6
+
+namespace CrackThePin;
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class CodeWars
+{
+    public static string crack(string hash)
+    {
+        hash = hash.ToUpper();
+        for (int k = 0; k <= 99999; ++k)
+        {
+            string pin = k.ToString("D5");
+            byte[] inputBytes = Encoding.ASCII.GetBytes(pin);
+            byte[] outputBytes = MD5.HashData(inputBytes);
+            string pinHash = Convert.ToHexString(outputBytes);
+
+            if (pinHash == hash)
+            {
+                return pin;
+            }
+        }
+
+        return "";
+    }
+}
