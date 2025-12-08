20dd2c09606e7e2aef81c167a063a0763cc440a0
Truman Hogg <hoggtruman@gmail.com>
2025-12-08 20:54:16 +0000

    Add SentenceCalculator

---
+++ 6 kyu/SentenceCalculator.cs
@@ -0,0 +1,29 @@
+﻿// https://www.codewars.com/kata/5970fce80ed776b94000008b
+
+namespace SentenceCalculator;
+
+public class Calculator
+{
+    public static int LettersToNumbers(string s)
+    {
+        int score = 0;
+
+        foreach (char c in s)
+        {
+            if(char.IsUpper(c))
+            {
+                score += 2 * (1 + c - 'A'); 
+            }
+            else if (char.IsLower(c))
+            {
+                score += 1 + c -'a';
+            }
+            else if (char.IsDigit(c))
+            {
+                score += c - '0';
+            }
+        }
+
+        return score;
+    }
+}
