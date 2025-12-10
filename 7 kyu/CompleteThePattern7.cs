845b1ec96e1494e65ca5f0a1df567fc5274b23a1
Truman Hogg <hoggtruman@gmail.com>
2025-12-10 03:53:04 +0000

    Add CompleteThePattern7

---
+++ 7 kyu/CompleteThePattern7.cs
@@ -0,0 +1,28 @@
+﻿// https://www.codewars.com/kata/557592fcdfc2220bed000042
+
+namespace CompleteThePattern7;
+
+using System.Text;
+
+public class CyclicalPermutation
+{
+    public static string Pattern(int n)
+    {
+        StringBuilder sb = new();
+
+        for (int k = 0; k < n; ++k)
+        {
+            for (int offset = 0; offset < n; ++offset)
+            {
+                sb.Append(1 + (k + offset) % n);
+            }
+
+            if (k != n - 1)
+            {
+                sb.Append('\n');
+            }                
+        }
+
+        return sb.ToString();
+    }
+}
