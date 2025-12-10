7c1878a5255dce69f0a6528056c99753fd92cc2a
Truman Hogg <hoggtruman@gmail.com>
2025-12-10 04:30:23 +0000

    Add MeanSquareError

---
+++ 5 kyu/MeanSquareError.cs
@@ -0,0 +1,18 @@
+﻿// https://www.codewars.com/kata/51edd51599a189fe7f000015
+
+namespace MeanSquareError;
+
+public class Kata
+{
+    public static double Solution(int[] first, int[] second)
+    {
+        double sum = 0;
+        for (int i = 0; i < first.Length; ++i)
+        {
+            int diff = first[i] - second[i];
+            sum += diff * diff;
+        }
+        
+        return sum / first.Length;
+    }
+}
