e26270a3227bc0c2a6cbcb88226edd909e06deb1
Truman Hogg <hoggtruman@gmail.com>
2025-12-10 00:35:48 +0000

    Add RoundAndRound

---
+++ 6 kyu/RoundAndRound.cs
@@ -0,0 +1,13 @@
+﻿// https://www.codewars.com/kata/5996eb39cdc8eb39f80000a0
+
+namespace RoundAndRound;
+
+using System;
+
+public static class RoundAndRound
+{
+    public static decimal RoundBy2DecimalPlaces(this decimal number)
+    {
+        return Math.Round(number, 2, MidpointRounding.AwayFromZero);
+    }
+}
