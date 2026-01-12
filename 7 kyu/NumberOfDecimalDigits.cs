0f794c15a66ac6bed21e1adc96505954f139a8e5
Truman Hogg <hoggtruman@gmail.com>
2026-01-12 17:45:26 +0000

    Add NumberOfDecimalDigits

---
+++ 7 kyu/NumberOfDecimalDigits.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/58fa273ca6d84c158e000052/csharp
+
+namespace NumberOfDecimalDigits;
+
+public class DecTools 
+{
+    public static int Digits(ulong n) 
+    {
+        return n <= 9 ? 1 : Digits(n / 10) + 1;
+    }
+}
