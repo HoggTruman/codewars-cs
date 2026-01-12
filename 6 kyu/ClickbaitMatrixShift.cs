251dc8b876dfe3cf7f6bacfbe96fd005f6ae14e3
Truman Hogg <hoggtruman@gmail.com>
2026-01-12 17:22:10 +0000

    Add ClickbaitMatrixShift

---
+++ 6 kyu/ClickbaitMatrixShift.cs
@@ -0,0 +1,29 @@
+﻿// https://www.codewars.com/kata/5afd3c451839f13b95000132
+
+namespace ClickbaitMatrixShift;
+
+public class JomoPipi
+{
+    public static char[][] Shift(char[][] m, int n)
+    {
+        int rows = m.Length;
+        int cols = m[0].Length;
+        char[][] result = new char[rows][];
+
+        for (int i = 0; i < rows; ++i)
+        {
+            result[i] = new char[cols];
+        }
+
+        for (int r = 0; r < rows; ++r)
+        {
+            for (int c = 0; c < cols; ++c)
+            {
+                int nextPos = (r * cols + c + n) % (rows * cols);
+                result[nextPos / cols][nextPos % cols] = m[r][c];
+            }
+        }
+
+        return result;
+    }
+}
