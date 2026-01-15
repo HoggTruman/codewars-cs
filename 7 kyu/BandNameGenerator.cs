b7616f6c0e4aabd7de07b88142bc2abbe43c94bb
Truman Hogg <hoggtruman@gmail.com>
2026-01-15 20:09:02 +0000

    Add BandNameGenerator

---
+++ 7 kyu/BandNameGenerator.cs
@@ -0,0 +1,28 @@
+﻿// https://www.codewars.com/kata/59727ff285281a44e3000011
+
+namespace BandNameGenerator;
+
+using System.Text;
+
+public class Kata
+{
+    public static string BandNameGenerator(string str)
+    {
+        StringBuilder sb = new();
+
+        if (str[0] == str[^1])
+        {
+            sb.Append(char.ToUpper(str[0]));
+            sb.Append(str[1..]);
+            sb.Append(str[1..]);
+        }
+        else
+        {
+            sb.Append("The ");
+            sb.Append(char.ToUpper(str[0]));
+            sb.Append(str[1..]);
+        }
+
+        return sb.ToString();
+    }
+}
