98284233fc47a50e52d7c7d792514525c18c83c0
Truman Hogg <hoggtruman@gmail.com>
2026-01-15 20:12:25 +0000

    Add Incrementer

---
+++ 7 kyu/Incrementer.cs
@@ -0,0 +1,16 @@
+﻿// https://www.codewars.com/kata/590e03aef55cab099a0002e8
+
+namespace Incrementer;
+
+public static class Kata
+{
+    public static int[] Incrementer(int[] numbers)
+    {
+        for (int i = 0; i < numbers.Length; ++i)
+        {
+            numbers[i] = (numbers[i] + i + 1) % 10;
+        }
+
+        return numbers;
+    }
+}
