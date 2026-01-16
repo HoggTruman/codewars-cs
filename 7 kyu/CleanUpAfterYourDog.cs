faf1471d9a4c67eb9546fc62e6eb457a09ab738a
Truman Hogg <hoggtruman@gmail.com>
2026-01-16 23:11:09 +0000

    Add CleanUpAfterYourDog

---
+++ 7 kyu/CleanUpAfterYourDog.cs
@@ -0,0 +1,25 @@
+﻿// https://www.codewars.com/kata/57faa6ff9610ce181b000028
+
+namespace CleanUpAfterYourDog;
+
+public class Kata
+{
+    public static string Crap(char[,] x, int bags, int cap)
+    {
+        int crapCount = 0;
+
+        for (int i = 0; i < x.GetLength(0); ++i)
+        {
+            for (int j = 0; j < x.GetLength(1); ++j)
+            {
+                if (x[i, j] == 'D')
+                    return "Dog!!";
+
+                if (x[i, j] == '@')
+                    ++crapCount;
+            }
+        }
+
+        return crapCount <= bags * cap ? "Clean": "Cr@p";
+    }
+}
