df5eaef3ad2a10c95c0e880358588bd48def64bd
Truman Hogg <hoggtruman@gmail.com>
2026-01-17 00:24:14 +0000

    Add ExcelSheetColumnNumbers

---
+++ 7 kyu/ExcelSheetColumnNumbers.cs
@@ -0,0 +1,21 @@
+﻿// https://www.codewars.com/kata/55ee3ebff71e82a30000006a
+
+namespace ExcelSheetColumnNumbers;
+
+using System;
+
+public class ExcelToNumber 
+{
+    public static long TitleToNumber(string title) 
+    {
+        long column = 0;
+
+        for (int i = 0; i < title.Length; ++i)
+        {
+            long value = title[title.Length - i - 1] - 'A' + 1;
+            column += value * (long)Math.Pow(26, i);
+        }
+
+        return column;
+    }
+}
