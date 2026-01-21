75b1d1ef67c6e5be0959c2dd8438dae522baa523
Truman Hogg <hoggtruman@gmail.com>
2026-01-21 20:49:51 +0000

    InterlockingBinaryPairs

---
+++ 7 kyu/InterlockingBinaryPairs.cs
@@ -0,0 +1,11 @@
+﻿// https://www.codewars.com/kata/628e3ee2e1daf90030239e8a
+
+namespace InterlockingBinaryPairs;
+
+public static class Kata
+{
+    public static bool Interlockable(ulong a, ulong b)
+    {
+        return (a & b) == 0;
+    }
+}
