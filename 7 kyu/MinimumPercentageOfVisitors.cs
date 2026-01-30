11d03ce413edf746472d3ededaf4d6ad21cf0a24
Truman Hogg <hoggtruman@gmail.com>
2026-01-30 16:31:12 +0000

    Add MinimumPercentageOfVistors

---
+++ 7 kyu/MinimumPercentageOfVisitors.cs
@@ -0,0 +1,14 @@
+﻿// https://www.codewars.com/kata/657e578bdc80170abd4dca79
+
+namespace MinimumPercentageOfVisitors;
+
+using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int MinimumPercentage(int[] foods)
+    {
+        return Math.Max(0, 100 - foods.Sum(x => 100 - x));
+    }
+}
