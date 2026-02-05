c382298bc316f0c0a4db0ad46135ab6a912b8eca
Truman Hogg <hoggtruman@gmail.com>
2026-02-05 13:54:04 +0000

    Add FindTheSlope

---
+++ 8 kyu/FindTheSlope.cs
@@ -0,0 +1,21 @@
+﻿// https://www.codewars.com/kata/55a75e2d0803fea18f00009d
+
+namespace FindTheSlope;
+
+using System;
+
+public class Slope
+{
+    public String slope(int[] points)
+    {
+        int deltax = points[2] - points[0];
+        int deltay = points[3] - points[1];
+
+        if (deltax == 0)
+        {
+            return "undefined";
+        }
+        
+        return (deltay / deltax).ToString();
+    }
+}
